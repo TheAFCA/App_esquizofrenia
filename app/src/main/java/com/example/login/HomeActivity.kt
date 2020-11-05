40015efa79bd108784ed1d7a29867e73daa196f1
TheAFCA <74022634+TheAFCA@users.noreply.github.com>
2020-11-05 16:03:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/login/HomeActivity.kt
@@ -0,0 +1,39 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_home.*
+
+enum class ProviderType {
+    BASIC
+}
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        // Setup
+
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+    }
+
+    private fun setup(email: String, provider: String) {
+
+        title = "Iniciio"
+        emailTextView.text = email
+        providerTextView.text = provider
+
+        logOutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+    }
+
+}
